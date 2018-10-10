e98735cefd8d25d3f4ecdf5862803cc67629ea13
Andrew Pett <astuart.pett@gmail.com>
2018-10-10 13:43:32 -0600

    Add test

--- test/dog_exceptex_test.exs
+++ test/dog_exceptex_test.exs
@@ -1,8 +1,23 @@
 defmodule DogExceptexTest do
-  use ExUnit.Case
-  doctest DogExceptex
+  use ExUnit.Case, async: false
+  import ExUnit.CaptureLog
 
-  test "greets the world" do
-    assert DogExceptex.hello() == :world
+  require Logger
+
+  test "sends an event to Dogstatsd when error is logged" do
+    {:ok, socket} = :gen_udp.open(60000, [:binary, {:active, true}])
+    Application.put_env(:logger, :dog_exceptex, [statsd_host: "127.0.0.1", statsd_port: 60000])
+
+    try do
+      Logger.add_backend(DogExceptex.Logger)
+
+      capture_log(fn -> Logger.error("A horrendous error occurred") end)
+
+      assert_receive {:udp, _port, {127, 0, 0, 1}, _port_n, message}, 1000
+      assert message =~ ~r/A horrendous error occurred/
+
+    after
+      :gen_udp.close(socket)
+    end
   end
 end
